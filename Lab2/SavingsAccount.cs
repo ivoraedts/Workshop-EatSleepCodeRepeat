132d1dc6ad2596c5a409b2217425fa1df67925a8
Ivo <User@PC76>
2016-10-04 18:14:49 +0200

    Refactored in SavingsAccount

--- Lab2/SavingsAccount.cs
+++ Lab2/SavingsAccount.cs
@@ -8,34 +8,47 @@ namespace eu.sig.training.ch04.v1
 
         public Transfer makeTransfer(string counterAccount, Money amount)
         {
-            // 1. Assuming result is 9-digit bank account number, validate 11-test:
-            if (String.IsNullOrEmpty(counterAccount) || counterAccount.Length != 9)
+            _validateStructure(counterAccount);
+            Transfer result = _makeTransfer(counterAccount, amount);
+            _validateResult(result);
+            return result;
+        }
+
+        private void _validateResult(Transfer result)
+        {
+            if (!result.CounterAccount.Equals(this.RegisteredCounterAccount))
             {
-                throw new BusinessException("Invalid account number!");
+                throw new BusinessException("Counter-account not registered!");
             }
+        }
+
+        private Transfer _makeTransfer(string counterAccount, Money amount)
+        {
+            CheckingAccount acct = Accounts.FindAcctByNumber(counterAccount);
+            Transfer result = new Transfer(this, acct, amount);
+            return result;
+        }
+
+        private static void _validateStructure(string counterAccount)
+        {
+            // 1. Assuming result is 9-digit bank account number, validate 11-test:
+            _checkLenght(counterAccount);
             int sum = 0;
             for (int i = 0; i < counterAccount.Length; i++)
             {
                 sum = sum + (9 - i) * (int)Char.GetNumericValue(counterAccount[i]);
             }
-            if (sum % 11 == 0)
-            {
-                // 2. Look up counter account and make transfer object:
-                CheckingAccount acct = Accounts.FindAcctByNumber(counterAccount);
-                Transfer result = new Transfer(this, acct, amount); // <2>
-                // 3. Check whether withdrawal is to registered counter account:
-                if (result.CounterAccount.Equals(this.RegisteredCounterAccount))
-                {
-                    return result;
-                }
-                else
-                {
-                    throw new BusinessException("Counter-account not registered!");
-                }
-            }
-            else
+            if (sum % 11 != 0)
             {
                 throw new BusinessException("Invalid account number!!");
+            }
+        }
+
+        private static void _checkLenght(string counterAccount)
+        {
+            if (String.IsNullOrEmpty(counterAccount) || counterAccount.Length != 9)
+            {
+                throw new BusinessException("Invalid account number!");
             }
         }
     }
