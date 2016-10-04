31b54838d55bbf21cd75a5e8a712f35c059c3057
Ivo <User@PC76>
2016-10-04 18:21:03 +0200

    Fixed checking account

--- Lab2/CheckingAccount.cs
+++ Lab2/CheckingAccount.cs
@@ -7,12 +7,15 @@ namespace eu.sig.training.ch04.v1
 
         public Transfer MakeTransfer(String counterAccount, Money amount)
         {
-            // 1. Check withdrawal limit:
-            if (amount.GreaterThan(this.transferLimit))
-            {
-                throw new BusinessException("Limit exceeded!");
-            }
+            _checkWithdrawalLimit(amount);
+            _checkElevenTest(counterAccount);
 
+            CheckingAccount acct = Accounts.FindAcctByNumber(counterAccount);
+            return new Transfer(this, acct, amount);
+        }
+
+        private static void _checkElevenTest(string counterAccount)
+        {
             // 2. Assuming result is 9-digit bank account number, validate 11-test:
             if (String.IsNullOrEmpty(counterAccount) || counterAccount.Length != 9)
             {
@@ -23,16 +26,18 @@ namespace eu.sig.training.ch04.v1
             {
                 sum = sum + (9 - i) * (int)Char.GetNumericValue(counterAccount[i]);
             }
-            if (sum % 11 == 0)
-            {
-                // 3. Look up counter account and make transfer object:
-                CheckingAccount acct = Accounts.FindAcctByNumber(counterAccount);
-                Transfer result = new Transfer(this, acct, amount);
-                return result;
-            }
-            else
+            if (sum % 11 != 0)
             {
                 throw new BusinessException("Invalid account number!");
+            }
+        }
+
+        private void _checkWithdrawalLimit(Money amount)
+        {
+            // 1. Check withdrawal limit:
+            if (amount.GreaterThan(this.transferLimit))
+            {
+                throw new BusinessException("Limit exceeded!");
             }
         }
     }
